ba0b5884aba624ee1385c7d689ba4d3385388c06
beatrizwiter <biawiter@gmail.com>
2023-05-11 22:35:19 -0300

    Primeiro Commit

---
+++ src/main.php
@@ -0,0 +1,12 @@
+<?php
+    require_once(__DIR__."/../vendor/autoload.php");
+    
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+
+    //$logger = new \Monolog\Logger("MinhaApp");
+    $logger = new Logger ("Aprendendo PDO");
+    $logger->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::DEBUG));
+
+    $logger->info("Meu primeiro log de verdade.");
+    
